e94bf8a05cb508a2fcc4e7ca81bdb4445bfb649e
nebojsa.ilic <nebojsa.ilic@pimcore.com>
2026-05-05 12:00:35 +0200

    Replaces manual docker-php-ext-install/pecl with mlocati/docker-php-extension-installer which properly handles runtime dependencies

--- files/build-cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-apt-get remove -y autoconf automake libtool make cmake ninja-build pkg-config build-essential g++ gcc libicu-dev;
-apt-get clean
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/* ~/.composer || true
-apt-get autoremove -y
-sync
-
